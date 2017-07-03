da65a0ea6d66d1d4efc3c434dfae90938e744a96
Roman Doroshenko <roman.doroshenko.tlt@gmail.com>
2017-07-03 09:43:50 +0400

    two entry points example

---
+++ dist/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Contact extends React.Component {
+  render() {
+    return <div>Contact us</div>
+  }
+}
+
+ReactDOM.render(<Contact />, document.getElementById('react-container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+// var webpack  = require('webpack');
+var path = require('path');
+
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    contact: './dist/contact',
+    about: './dist/about'
+  },
   output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js'
+    path: path.join(__dirname,'build'),
+    filename: '[name].bundle.js'
   },
   module: {
     loaders: [
